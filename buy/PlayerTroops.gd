8fb553668720cf8544b42161cdd2c3d9fe50fcb9
Andre Ferrari Moukarzel <andremoukarzel@gmail.com>
2024-06-26 19:44:09 -0300

    Add platforms and lights

--- buy/PlayerTroops.gd
+++ buy/PlayerTroops.gd
@@ -4,6 +4,7 @@ extends Node3D
 signal position_already_taken(pos: int)
 signal unit_added
 
+const ON_LIGHT_ENERGY: float = 7.8
 
 var currently_on: int = -1 # Position the mouse is currently hovering on
 var units: Array = [null, null, null, null, null, null]
@@ -14,6 +15,8 @@ func _ready():
 		var PosNodeArea: Area3D = get_node("Position" + str(i) + "/Area3D")
 		PosNodeArea.mouse_entered.connect(mouse_inside.bind(i))
 		PosNodeArea.mouse_exited.connect(mouse_left.bind(i))
+		var PosNodeLight: SpotLight3D = get_node("Position" + str(i) + "/SpotLight3D")
+		PosNodeLight.light_energy = 0
 
 
 func mouse_inside(pos: int):
@@ -29,6 +32,18 @@ func get_current_pos():
 	return currently_on
 
 
+func turn_light_on(pos: int):
+	var PosNodeLight: SpotLight3D = get_node("Position" + str(pos) + "/SpotLight3D")
+	var tween = get_tree().create_tween()
+	tween.tween_property(PosNodeLight, "light_energy", ON_LIGHT_ENERGY, .7)
+
+
+func turn_light_off(pos: int):
+	var PosNodeLight: SpotLight3D = get_node("Position" + str(pos) + "/SpotLight3D")
+	var tween = get_tree().create_tween()
+	tween.tween_property(PosNodeLight, "light_energy", 0.0, 1)
+
+
 func add_unit(unit, pos: int):
 	if pos >= len(units):
 		push_error("Invalid unit position")
@@ -38,6 +53,7 @@ func add_unit(unit, pos: int):
 		emit_signal("position_already_taken", pos)
 		return
 	
+	turn_light_on(pos)
 	var PosNode: Node3D = get_node("Position" + str(pos))
 	PosNode.add_child(unit)
 	units[pos] = unit
@@ -50,4 +66,4 @@ func add_unit(unit, pos: int):
 func remove_unit(pos: int):
 	get_node("Position" + str(pos)).remove_child(units[pos])
 	units[pos] = null
-
+	turn_light_off(pos)
